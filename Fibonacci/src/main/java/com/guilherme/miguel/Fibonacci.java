823f434bfe8fdf98056cb83c6274f54c293184da
Miguel Guilherme <mguilherme82@gmail.com>
2017-05-07 23:42:46 +0100

    Refactor fibonacciRecursive

--- Fibonacci/src/main/java/com/guilherme/miguel/Fibonacci.java
+++ Fibonacci/src/main/java/com/guilherme/miguel/Fibonacci.java
@@ -16,8 +16,7 @@ public class Fibonacci {
      * @return Fibonacci number
      */
     public static int fibonacciRecursive(int n) {
-        if (n <= 1) return n;
-        return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+        return (n <= 1) ? n : fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
     }
 
     /**
