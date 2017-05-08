6ab743c33687b1efba7d0eb19433d42df6af0041
Miguel Guilherme <mguilherme82@gmail.com>
2017-05-08 01:27:55 +0100

    Refactor tests

--- Fibonacci/src/test/java/com/guilherme/miguel/FibonacciTest.java
+++ Fibonacci/src/test/java/com/guilherme/miguel/FibonacciTest.java
@@ -3,8 +3,10 @@ package com.guilherme.miguel;
 
 import org.junit.Test;
 
-import static com.guilherme.miguel.Fibonacci.fibonacciIterative;
-import static com.guilherme.miguel.Fibonacci.fibonacciRecursive;
+import java.util.function.IntUnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -12,27 +14,39 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class FibonacciTest {
 
-    public final static String EXPECTED = "0 1 1 2 3 5 8 13 21 34 55 89 144 233 377";
+    public final static String EXPECTED = "0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377";
 
+    /**
+     * Iterative Test
+     */
     @Test
     public void testIterative() throws Exception {
-        String output = "";
-        for (int i = 0; i < 15; i++) {
-            output += fibonacciRecursive(i) + " ";
-        }
-        output = output.trim();
+        String output = getOutput(Fibonacci::fibonacciIterative);
 
         assertThat(output).isEqualTo(EXPECTED);
     }
 
+    /**
+     * Recursive test
+     */
     @Test
     public void testRecursive() throws Exception {
-        String output = "";
-        for (int i = 0; i < 15; i++) {
-            output += fibonacciIterative(i) + " ";
-        }
-        output = output.trim();
+        String output = getOutput(Fibonacci::fibonacciRecursive);
 
         assertThat(output).isEqualTo(EXPECTED);
     }
+
+    /**
+     * Generate a String with numbers from 0 to 14 for a given operation
+     *
+     * @param operation the operation
+     * @return A comma separated String
+     */
+    private String getOutput(IntUnaryOperator operation) {
+        return IntStream.range(0, 15)
+                .map(operation)
+                .mapToObj(Integer::toString)
+                .collect(Collectors.joining(", "));
+    }
+
 }
