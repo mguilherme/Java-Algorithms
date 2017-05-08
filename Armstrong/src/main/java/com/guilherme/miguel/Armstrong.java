84e6cb06c7315270eed9cc3f20c26bb30ad377a8
Miguel Guilherme <mguilherme82@gmail.com>
2017-05-08 22:42:49 +0100

    Refactor Armstrong

--- Armstrong/src/main/java/com/guilherme/miguel/Armstrong.java
+++ Armstrong/src/main/java/com/guilherme/miguel/Armstrong.java
@@ -12,12 +12,12 @@ public class Armstrong {
      * @return true if Armstrong, false otherwise
      */
     public static boolean isArmstrong(int number) {
-        int length = String.valueOf(number).length();
+        String value = String.valueOf(number);
         int sum = 0;
 
-        for (int i = 0; i < length; i++) {
-            int digit = Character.getNumericValue(Integer.toString(number).charAt(i));
-            sum += Math.pow(digit, length);
+        for (int i = 0; i < value.length(); i++) {
+            int digit = Character.getNumericValue(value.charAt(i));
+            sum += Math.pow(digit, value.length());
         }
 
         return number == sum;
